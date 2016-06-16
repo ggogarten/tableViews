3a9d9b554b4aebfe91d67c41a42bda0066d709d2
George Gogarten <georgegogarten@georges-macbook-air.local>
2016-06-16 19:07:52 -0400

    added delegate

--- Table Views/ViewController.swift
+++ Table Views/ViewController.swift
@@ -8,13 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+ 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+        return 3
+    
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
